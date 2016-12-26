124714ff5079f384b1d96526b37485168736aa17
Seganok <msn3862@mail.ru>
2016-12-26 19:15:27 +0300

    пробуем сидить хероку3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,21 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->delete();
-        $json = File::get("database/data/Lots.json");
-        $data = json_decode($json);
-        foreach ($data as $obj) {
-          Task::create(array(
-            'id' => $obj->id,
-            'title' => $obj->title,
-            'slug' => $obj->slug,
-            'izmer' => $obj->izmer,
-            'price' => $obj->price,
-            'otovare' => $obj->otovare,
-            'published' => $ogj->published,
-            'image' => $obj->image,
-            'type' => $obj->type
-          ));
-        }
+      Eloquent::unguard();
+      $this->call('UserTableSeeder');
+      $this->command->info('User table seeded!');
+      $path = '/database/data/tasks.sql';
+      DB::Task(file_get_contents($path));
+
     }
 }
