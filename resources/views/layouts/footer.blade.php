44056036de958fc5f07ff5399e069732e3259a38
Seganok <msn3862@mail.ru>
2017-01-04 21:23:48 +0300

    Настраиваем SEO_2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,10 @@
 <footer>
 	<div class="footer container">
 		<p>© 2011 - 2016 СПД ФЛ Шиленко</p>
-		<p style="text-align: right; font-size: 10px;">
+
+		<q style="float: left; font-size: 10px;">Севастополь, ул.Курганная 6</q>
+		<q style="float: right; font-size: 10px;">
 			Аренда строительных лесов и опалубки в Севастополе.
-		</p>
+		</q>
 	</div>	
 </footer>
